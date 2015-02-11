9cef336d46830de969fb8dc0822732a812031f79
Ali Homafar <home.isfar@gmail.com>
2015-02-10 21:46:12 -0600

    Weird bug in python

--- CacheGenerator.py
+++ CacheGenerator.py
@@ -3,27 +3,60 @@ import json
 import sys
 import os
 
-# infile = open('/home/alih/school/cs373-SWE/netflix/netflix/training_set/mv_0002734.txt', 'r')
+from collections import Counter
+
+movieList = open('/home/alih/school/cs373-SWE/netflix/netflix/movie_titles.txt', 'r')
+movieDict = {}
+
+for line in movieList :
+	splitLine = line.split(",")
+	#print (splitLine)
+	movieDict[splitLine[0]] = [splitLine[1]]
 
 
-ratinglist = []
-moviedict = {}
-for root, dirs, files in os.walk('/home/alih/school/cs373-SWE/netflix/netflix/training_set/'):
+x = 0
+
+for root, dirs, files in os.walk('/home/alih/school/cs373-SWE/netflix/netflix/training_set/') :
+
+	# Go through every file in the training set
 	for filename in files:
+
+		if x == 3 :
+			break
+		
+		# infoList represents multiple bits of data. [0] is the average, [1] is the mode, [2]
+		#infoList = []
+		# list of every rating a movie has received
+		ratingList = []
 	
 		infile = open(root + filename, 'r')
-
+		# The first line (Movie ID) has the colon removed and is converted to an int
 		movie = int(infile.readline().split(":")[0])
 
+		#Store every rating in the list
 		for line in infile:
 			splitline = line.split(",")
-			ratinglist.append (int(splitline[1]))
+			ratingList.append (int(splitline[1]))
 
-		moviedict[movie] = ratinglist
+		average = sum(ratingList) / len(ratingList)
 
-#for line in infile:
-#	print (line, end="")
+		average = round(average,3)
 
 
-#with open('output', 'w') as outfile:
-#	json.dump(mv_0002734, outfile)
+		mode = Counter(ratingList)
+
+		movieDict[movie].append(average)
+		movieDict[movie].append(mode.most_common(1)[0])
+#		infoList.append(ratingList)
+
+#		movieDict[movie] = infoList
+
+		x += 1
+
+with open ('output', 'w') as outfile:
+	json.dump (movieDict, outfile)
+
+testinfile = open('output', 'r')
+moviedict2 = json.loads(testinfile.read())
+print (moviedict2)
+print ("Done")
