cd06e1354ec807ac8cb794973903ae1e282a7acd
larry-liu <liudi1990@utexas.edu>
2015-02-19 17:46:10 -0600

    hit it to 0.97

--- Netflix.py
+++ Netflix.py
@@ -5,19 +5,20 @@ def netflix_cal():
     Ucache = json.load(open('/u/mck782/netflix-tests/pma459-usrAvgCache.json', 'r'))
     Mcache = json.load(open('/u/mck782/netflix-tests/pma459-mvAvgCache.json', 'r'))
     Acache = json.load(open('/u/mck782/netflix-tests/pma459-answersCache.json', 'r'))
-
+    Ycache = json.load(open('/u/mck782/netflix-tests/af22574-movieDates.json', 'r'))
+    MYcache = json.load(open('/u/mck782/netflix-tests/cdm2697-userRatingsAveragedOver10yInterval-v2.json', 'r'))
     result = []
     answer = []
-#    probe = open('probe.txt', 'r')
     probe = sys.stdin
     currentmid = '1'
     a = []
     b = []
 
     for line in probe:
+        f = 0
         if len(line.split(':')) > 1:
              currentmid = line.split(':')[0]
-             print (line, end="")
+             #print (line, end="")
         else:
              currentuid = line.split('\n')[0]
              r = Acache[currentmid][currentuid]
@@ -28,22 +29,29 @@ def netflix_cal():
                  val3 = 5.0
              if val3 < 1 :
                  val3 = 1.0
-
-             print("%.1f" % val3)
-
-
-             a += (Ucache[currentuid],)
-             b += (Mcache[int(currentmid)],)
+             f = []
+             if (currentmid in Ycache and not Ycache[currentmid] == 'NULL'):
+                 if (currentuid in MYcache):
+                     year = int(Ycache[currentmid])
+                     year = (year // 10) * 10
+                     for l in MYcache[currentuid]:
+                         if l[0] == year: 
+                             f += (val3 * 0.5 + 0.5*l[1],)
+  
+             if (f == []):
+                 result += (val3,)
+             else:
+                 result += (f[0],)
+             #print("%.1f" % val3)
 
              answer += (r,)
 
-    mixture = add(multiply(b, 0.5) + multiply(0.7, a), -0.7)
-    for i in range(len(mixture)):
-        if (mixture[i] > 5):
-            mixture[i] = 5
-        if (mixture[i] < 1):
-            mixture[i] = 1
-    print ("RMSE: %.2f" % sqrt(mean(square(subtract(mixture, answer)))))
+    for i in range(len(result)):
+        if (result[i] > 5):
+            result[i] = 5
+        if (result[i] < 1):
+            result[i] = 1
+    print (sqrt(mean(square(subtract(result, answer)))))
 
 
 
