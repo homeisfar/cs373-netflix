b8c89e16cbe71372b79d03c66ddc000950e1ae31
larry-liu <liudi1990@utexas.edu>
2015-02-19 19:35:22 -0600

    test cases for load_cache() and read()(All passed)

--- TestNetflix.py
+++ TestNetflix.py
@@ -4,16 +4,14 @@
 
 from io         import StringIO
 from unittest   import main, TestCase
-from Netflix    import netflix_cal
-
+from Netflix    import netflix_load_cache, netflix_read, netflix_cal, netflix_solve
+import sys, json
 
 class TestNetflix (TestCase) :
 
     def test_netflix_cal_1 (self) :
         r = StringIO( "1:\n30878\n2647871\n1283744\n2488120\n317050\n1904905\n1989766\n")
         w = StringIO()
-
-
 
     def test_netflix_load_cache_1(self) :
         cache = netflix_load_cache();
@@ -33,13 +31,25 @@ class TestNetflix (TestCase) :
 
     def test_netflix_load_cache_5(self) :
         cache = netflix_load_cache();
-        self.assertEqual(cache[4], MYcache = json.load(open('/u/mck782/netflix-tests/cdm2697-userRatingsAveragedOver10yInterval-v2.json', 'r')))
+        self.assertEqual(cache[4], json.load(open('/u/mck782/netflix-tests/cdm2697-userRatingsAveragedOver10yInterval-v2.json', 'r')))
 
     def test_netflix_read_1(self) :
+        self.assertEqual(netflix_read("1:\n")[0], True)
+        self.assertEqual(netflix_read("1:\n")[1], "1")
 
     def test_netflix_read_2(self) :
+        self.assertEqual(netflix_read("1\n")[0], False)
+        self.assertEqual(netflix_read("1\n")[1], "1")
 
     def test_netflix_read_3(self) :
+        self.assertEqual(netflix_read("11111\n")[1], "11111")
+
+    def test_netflix_read_4(self) :
+        self.assertEqual(netflix_read("11111:\n")[1], "11111")
+
+
+
+
 
 
 
